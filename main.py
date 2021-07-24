056255dd38788401c385fb1a4fd6077b08060d33
Trevor Tang <awpdata@gmail.com>
2021-07-24 13:26:02 -0700

    Added custom board size support

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 # Write your code here
-def check(coords):
+def check(coords, top, bottom=1):
     for num in coords:
-        if num.isalpha() or len(coords) != 2 or int(num) < 1 or int(num) > 8:
+        if num.isalpha() or len(coords) != 2 or int(num) < bottom or int(num) > top:
             return False
     return True
 
 
-# just a test below
+print("Enter your board dimensions")
+dim = input().split()
+while not check(dim, 10):
+    print("Invalid dimensions!\n")
+    print("Enter your board dimensions")
+    dim = input().split()
+
+rows = int(dim[0])
+columns = int(dim[1])
 print("Enter the knight's starting position")
 coord = input().split()
-if not check(coord):
+if not check(coord, columns):
     print("Invalid dimensions!")
 else:
     x = int(coord[0])
     y = int(coord[1])
-    print(" -------------------")
-    for i in range(8, 0, -1):
+    cell_length = len(str(int(columns) * int(rows)))
+    print(" {}".format("-" * (rows * (cell_length + 1) + 3)))
+    for i in range(columns, 0, -1):
         print(str(i) + "|", end="")
-        for j in range(1, 9):
-            print(" X", end="") if j == x and i == y else print(" _", end="")
+        for j in range(1, rows + 1):
+            if j == x and i == y:
+                print(" " * cell_length + "X", end="")
+            else:
+                print(" {}".format("_" * cell_length), end="")
         print(" |")
-    print(" -------------------")
-    print("   1 2 3 4 5 6 7 8")
+    print(" {}".format("-" * (rows * (cell_length + 1) + 3)))
+    print("  ", end="")
+    for i in range(1, rows + 1):
+        print(" " * cell_length + "{}".format(i), end="")
