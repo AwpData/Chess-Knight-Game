6fd98c63dd475027dffa9d5702a90addc03fe650
Trevor Tang <awpdata@gmail.com>
2021-07-24 14:55:48 -0700

    Code cleanup

--- main.py
+++ main.py
@@ -6,6 +6,33 @@ def check(coords, top, bottom=1):
     return True
 
 
+def get_coordinates():
+    coord = input().split()
+    if not check(coord, columns):
+        print("Invalid dimensions!")
+        return "Failed"
+    return coord
+
+
+def draw_board(coordinates, cols, row):
+    x = int(coordinates[0])
+    y = int(coordinates[1])
+    cell_length = len(str(int(cols) * int(row)))
+    print(" {}".format("-" * (row * (cell_length + 1) + 3)))
+    for i in range(cols, 0, -1):
+        print(str(i) + "|", end="")
+        for j in range(1, row + 1):
+            if j == x and i == y:
+                print(" " * cell_length + "X", end="")
+            else:
+                print(" {}".format("_" * cell_length), end="")
+        print(" |")
+    print(" {}".format("-" * (row * (cell_length + 1) + 3)))
+    print("  ", end="")
+    for i in range(1, row + 1):
+        print(" " * cell_length + "{}".format(i), end="")
+
+
 print("Enter your board dimensions")
 dim = input().split()
 while not check(dim, 10):
@@ -15,24 +42,8 @@ while not check(dim, 10):
 
 rows = int(dim[0])
 columns = int(dim[1])
+
 print("Enter the knight's starting position")
-coord = input().split()
-if not check(coord, columns):
-    print("Invalid dimensions!")
-else:
-    x = int(coord[0])
-    y = int(coord[1])
-    cell_length = len(str(int(columns) * int(rows)))
-    print(" {}".format("-" * (rows * (cell_length + 1) + 3)))
-    for i in range(columns, 0, -1):
-        print(str(i) + "|", end="")
-        for j in range(1, rows + 1):
-            if j == x and i == y:
-                print(" " * cell_length + "X", end="")
-            else:
-                print(" {}".format("_" * cell_length), end="")
-        print(" |")
-    print(" {}".format("-" * (rows * (cell_length + 1) + 3)))
-    print("  ", end="")
-    for i in range(1, rows + 1):
-        print(" " * cell_length + "{}".format(i), end="")
+start = get_coordinates()
+if start != "Failed":
+    draw_board(start, columns, rows)
